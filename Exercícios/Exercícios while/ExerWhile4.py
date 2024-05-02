2e91194e21f8cd8dbe52eb336fec4ff05b9674b1
Dudisdu99 <131018122+Dudisdud99@users.noreply.github.com>
2024-05-02 17:27:41 +0000

    exer de while

---
+++ Exercícios/Exercícios while/ExerWhile4.py
@@ -0,0 +1,5 @@
+x=1
+while x<=10:
+    n = int(input("Digite um numero: "))
+    print(n/2)
+    x+=1
---
+++ Exercícios/Exercícios while/ExerWhile5.py
@@ -0,0 +1,5 @@
+x=1
+while x<=5:
+    n = int(input("Digite um numero: "))
+    print(n**3)
+    x+=1
